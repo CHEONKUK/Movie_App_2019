aa21e37a08c4699a0fc4beebddc4647b9ad416e3
CHEONKUK <dd1139001@naver.com>
2019-10-04 13:45:52 +0900

    191004
    # ReactJS Study

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+// Component2
+function Movie({ name }) {
+  return <h4>EMOJI :: {name} </h4>;
+}
+
+// Component1
 function App() {
-  return <div className="App" />;
+  return (
+    <div>
+      <h2>Hello</h2>
+      <Movie name="😍" />
+    </div>
+  );
 }
 
 export default App;
