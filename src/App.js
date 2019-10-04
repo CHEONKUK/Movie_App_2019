312138934817236a7cd2afd44a9eecdc69380ab1
CHEONKUK <dd1139001@naver.com>
2019-10-04 14:41:52 +0900

    191004
    # ReactJS Study

--- src/App.js
+++ src/App.js
@@ -13,22 +13,30 @@ function Movie({ name, img }) {
 
 const emojiLove = [
   {
+    id: 1,
     name: "love",
     image: "😍"
   },
   {
+    id: 2,
     name: "sad",
     image: "🥶"
   },
   {
+    id: 3,
     name: "angry",
     image: "😈"
   },
   {
+    id: 4,
     name: "wow",
     image: "😻"
   }
 ];
+
+function renderEmoji(item) {
+  return <Movie key={item.id} name={item.name} img={item.image} />;
+}
 
 // Component1
 function App() {
@@ -36,9 +44,7 @@ function App() {
     <div>
       <h2>App function</h2>
       {/* item은 Object (item의 이름은 dish, lol..등 다른 이름으로도 변경 가능) */}
-      {emojiLove.map(item => (
-        <Movie name={item.name} img={item.image} />
-      ))}
+      {emojiLove.map(renderEmoji)}
     </div>
   );
 }
