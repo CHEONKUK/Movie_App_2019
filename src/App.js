48941d06c2b723be8ee56b3249f3aa60e1b271a5
CHEONKUK <dd1139001@naver.com>
2019-10-04 14:28:23 +0900

    191004
    # ReactJS Study

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from "react";
 
 // Component2
-function Movie({ name }) {
-  return <h4>EMOJI :: {name} </h4>;
+function Movie({ name, img }) {
+  return (
+    <div>
+      <h2>
+        EMOJI : {name} --> {img}
+      </h2>
+    </div>
+  );
 }
+
+const emojiLove = [
+  {
+    name: "love",
+    image: "😍"
+  },
+  {
+    name: "sad",
+    image: "🥶"
+  },
+  {
+    name: "angry",
+    image: "😈"
+  },
+  {
+    name: "wow",
+    image: "😻"
+  }
+];
 
 // Component1
 function App() {
   return (
     <div>
-      <h2>Hello</h2>
-      <Movie name="😍" />
-      <Movie name="😁" />
-      <Movie name="😈" />
-      <Movie name="🥶" />
+      <h2>App function</h2>
+      {/* item은 Object (item의 이름은 dish, lol..등 다른 이름으로도 변경 가능) */}
+      {emojiLove.map(item => (
+        <Movie name={item.name} img={item.image} />
+      ))}
     </div>
   );
 }
