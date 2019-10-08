2ea6f6145c1ce9dd5cce29c5fb2062d6f998c31b
CHEONKUK <dd1139001@naver.com>
2019-10-08 21:37:18 +0900

    191008
    # ReactJS Study

--- src/App.js
+++ src/App.js
@@ -1,53 +1,27 @@
+/* eslint-disable */
 import React from "react";
-import PropTypes from "prop-types";
 
-const emojiLove = [
-  {
-    id: 1,
-    name: "love",
-    img: "😍"
-  },
-  {
-    id: 2,
-    name: "sad",
-    img: "🥶"
-  },
-  {
-    id: 3,
-    name: "angry",
-    img: "😈"
-  },
-  {
-    id: 4,
-    name: "wow",
-    img: "😻"
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    console.log("+");
+  };
+  min = () => {
+    console.log("-");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number : {this.state.count}</h1>
+        <button onClick={this.add}> + </button>
+        <button onClick={this.min}> - </button>
+      </div>
+    );
   }
-];
-
-// Component2
-function Movie({ name, img }) {
-  return (
-    <div>
-      <h2>
-        EMOJI : {name} --> {img}
-      </h2>
-    </div>
-  );
-}
-
-function renderEmoji(item) {
-  return <Movie key={item.id} name={item.name} img={item.img} />;
-}
-
-// Component1
-function App() {
-  return (
-    <div>
-      <h2>App function</h2>
-      {/* item은 Object (item의 이름은 dish, lol..등 다른 이름으로도 변경 가능) */}
-      {emojiLove.map(renderEmoji)}
-    </div>
-  );
 }
 
 export default App;
