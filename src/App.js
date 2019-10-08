4ae4244c5148a84d84dcecfca8c7114f784dd119
CHEONKUK <dd1139001@naver.com>
2019-10-08 22:29:37 +0900

    191008
    #ReactJS Study

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App extends React.Component {
   };
 
   add = () => {
-    console.log("+");
+    this.setState(current => ({ count: current.count + 1 }));
   };
   min = () => {
-    console.log("-");
+    this.setState(current => ({ count: current.count - 1 }));
   };
 
   render() {
