92aa43c8a222392485e9eb5cf60f1208141f5805
CHEONKUK <dd1139001@naver.com>
2019-10-10 16:11:24 +0900

    191010
    # ReactJS movieApp Study

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 /* eslint-disable */
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movie: []
+  };
+
+  getMovies = async () => {
+    const moives = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(moives.data.data.moives);
   };
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
