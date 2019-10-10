ac6dc6ffabb89b441b9589c4c3340933e864a3ba
CHEONKUK <dd1139001@naver.com>
2019-10-10 15:52:53 +0900

    191010
    # ReactJS movieApp Study

--- src/App.js
+++ src/App.js
@@ -1,43 +1,20 @@
 /* eslint-disable */
 import React from "react";
-import A from "./A";
-import LifeCycleAPI from "./LifeCycleAPI";
 
 class App extends React.Component {
   state = {
-    num: 1,
-    error: false
+    isLoading: true
   };
 
-  handleClick = () => {
-    this.setState({ num: this.state.num + 1 });
-  };
-
-  componentDidCatch(error, info) {
-    console.log("# Error  : " + error);
-    console.log("# Info   : " + info.value);
-    this.setState({
-      error:true
-    })
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
 
   render() {
-    
-    if(this.state.error){
-      return(
-        <div>ERROR</div>
-      )
-    }
-    
-    return(
-      <div>
-        <br />
-        <A />
-        <br />
-        <LifeCycleAPI value={this.state.num} />
-        <button onClick={this.handleClick}>ClickMe</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are Ready"}</div>;
   }
 }
 
