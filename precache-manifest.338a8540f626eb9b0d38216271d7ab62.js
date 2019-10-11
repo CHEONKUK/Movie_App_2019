24954a33968d0d21be59bd105c098fee2068e159
CHEONKUK <dd1139001@naver.com>
2019-10-11 13:27:12 +0900

    Updates

---
+++ precache-manifest.338a8540f626eb9b0d38216271d7ab62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73d76b5c76c11ff6cf2ec724edaed22",
+    "url": "/CHEONKUK/movie_app_2019/index.html"
+  },
+  {
+    "revision": "420b11177bb9cee04a27",
+    "url": "/CHEONKUK/movie_app_2019/static/css/main.2ced33d5.chunk.css"
+  },
+  {
+    "revision": "5f0e47690423a5e775c7",
+    "url": "/CHEONKUK/movie_app_2019/static/js/2.77726bf0.chunk.js"
+  },
+  {
+    "revision": "420b11177bb9cee04a27",
+    "url": "/CHEONKUK/movie_app_2019/static/js/main.13b0a1db.chunk.js"
+  },
+  {
+    "revision": "6f0524ce33b490a9d07a",
+    "url": "/CHEONKUK/movie_app_2019/static/js/runtime-main.52b930ca.js"
+  }
+]);
